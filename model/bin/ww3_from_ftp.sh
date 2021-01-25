4c642133d08ca1fbad40df5e7170bdce204472d7
Ty Hesser <thesser1@gmail.com>
2021-01-25 02:40:23 -0600

    updating waterlevel files in ftp script for beors regtest

--- model/bin/ww3_from_ftp.sh
+++ model/bin/ww3_from_ftp.sh
@@ -53,6 +53,7 @@ cp -r data_regtests/ww3_tp2.14/input/toy/*.nc regtests/ww3_tp2.14/input/toy/
 cp -r data_regtests/ww3_tp2.17/input/*     regtests/ww3_tp2.17/input/
 mkdir regtests/ww3_tp2.19/input
 cp -r data_regtests/ww3_tp2.19/input/*.msh regtests/ww3_tp2.19/input/
+cp -r data_regtests/ww3_tp2.19/input/*.nc regtests/ww3_tp2.19/input/
 cp -r data_regtests/ww3_tp2.19/input/boundary1A.nc regtests/ww3_tp2.19/input_Case1A/
 cp -r data_regtests/ww3_tp2.19/input/boundary1B.nc regtests/ww3_tp2.19/input_Case1B/
 cp -r data_regtests/ww3_tp2.19/input/boundary1C.nc regtests/ww3_tp2.19/input_Case1C/
